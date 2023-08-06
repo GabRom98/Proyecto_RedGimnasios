06bac076e263374c03228f2c572db18bc593dc49
FedericoLazarte <fedelazarte2015@gmail.com>
2023-08-06 18:33:42 -0300

    GA-28-Crear-Province-Repository (se agrego la importacion de la clase Province)

--- src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/repository/ProvinceRepository.java
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/repository/ProvinceRepository.java
@@ -2,7 +2,7 @@ package ProyectoATLbootcamp.Proyecto_RedGimnasios.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.Province;
 @Repository
 public interface ProvinceRepository extends CrudRepository<Province, Long> {
 }
