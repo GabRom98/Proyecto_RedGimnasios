b386a76995582cbd1ab0d1651c282b242989c917
Gabriel Francisco Romero Rocha <109051710+GabRom98@users.noreply.github.com>
2023-08-07 16:29:34 -0300

    GA-33 se agrego repositorio con CrudRepository de GymClass

---
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/repository/GymClassRepository.java
@@ -0,0 +1,8 @@
+package ProyectoATLbootcamp.Proyecto_RedGimnasios.repository;
+
+
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.GymClass;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GymClassRepository extends CrudRepository<GymClass, Long> {
+}
