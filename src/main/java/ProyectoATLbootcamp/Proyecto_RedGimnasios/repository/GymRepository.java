33019fcc4074294b6743fe7c5531cb516245fa7e
David <daviddanielc25@gmail.com>
2023-08-08 14:07:35 -0300

    GA-27 Crear GymRepository

---
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/repository/GymRepository.java
@@ -0,0 +1,7 @@
+package ProyectoATLbootcamp.Proyecto_RedGimnasios.repository;
+
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.Gym;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GymRepository extends CrudRepository<Gym,Long> {
+}
