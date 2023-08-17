3355322b31e5e5cbca6fdec4be32e0c607cf7ba5
Juan Jose Ceballos Navia <juanfix@gmail.com>
2023-08-17 12:12:19 -0500

    Service and controller of GymActivity added

--- src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/controllers/GymActivityController.java
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/controllers/GymActivityController.java
@@ -1,13 +1,42 @@
 package ProyectoATLbootcamp.Proyecto_RedGimnasios.controllers;
 
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.GymActivity;
 import ProyectoATLbootcamp.Proyecto_RedGimnasios.services.GymActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/")
 public class GymActivityController {
 
     @Autowired
     GymActivityService gymActivityService;
+
+    @GetMapping("gymActivities")
+    public List<GymActivity> getAllGymActivities(){
+        return gymActivityService.listAllGymActivities();
+    }
+
+    @GetMapping("gymActivities/{id}")
+    public GymActivity getGymActivityById(@PathVariable Long id){
+        return gymActivityService.findOneGymActivity(id);
+    }
+
+    @PostMapping("gymActivities")
+    public GymActivity createGymActivity(@RequestBody GymActivity gymActivity){
+        return gymActivityService.createGymActivity(gymActivity);
+    }
+
+    @PutMapping("gymActivities/{id}")
+    public GymActivity updateGymActivity(@PathVariable Long id, @RequestBody GymActivity gymActivity){
+        return gymActivityService.updateGymActivity(id,gymActivity);
+    }
+
+    @DeleteMapping("gymActivities/{id}")
+    public void deleteGymActivity(@PathVariable Long id){
+        gymActivityService.deleteGymActivity(id);
+    }
 
 }
--- src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/services/GymActivityService.java
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/services/GymActivityService.java
@@ -1,13 +1,40 @@
 package ProyectoATLbootcamp.Proyecto_RedGimnasios.services;
 
 import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.GymActivity;
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.repository.GymActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GymActivityService {
 
     @Autowired
-    GymActivity gymActivity;
+    GymActivityRepository gymActivityRepository;
+
+    public List<GymActivity> listAllGymActivities(){
+        return (List<GymActivity>) gymActivityRepository.findAll();
+    }
+
+    public GymActivity findOneGymActivity(Long id){
+        return gymActivityRepository.findById(id).orElse(null);
+    }
+
+    public GymActivity createGymActivity(GymActivity gymActivity){
+        return gymActivityRepository.save(gymActivity);
+    }
+
+    public GymActivity updateGymActivity(Long id, GymActivity gymActivity){
+        if (gymActivityRepository.existsById(id)){
+            gymActivity.setId(id);
+            return gymActivityRepository.save(gymActivity);
+        }
+        return null;
+    }
+
+    public void deleteGymActivity(Long id){
+        gymActivityRepository.deleteById(id);
+    }
 
 }
