af12715b6ee9f44f8103c2c343f3e80f50c28965
Hero Molina Sanchez <heromolina@gmail.com>
2023-08-24 16:13:10 -0600

    GA-50_Service_y_Controller_Province

--- src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/controllers/ProvinceController.java
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/controllers/ProvinceController.java
@@ -1,13 +1,39 @@
 package ProyectoATLbootcamp.Proyecto_RedGimnasios.controllers;
 
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.Activity;
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.Province;
 import ProyectoATLbootcamp.Proyecto_RedGimnasios.services.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProvinceController {
 
     @Autowired
     ProvinceService provinceService;
+
+    @GetMapping("/api/gimnasio/province/{id}")
+    public Province getProvince(@PathVariable Long id) {
+
+        return provinceService.getProvince(id);
+    }
+
+    @GetMapping("/api/gimnasio/province")
+    public List<Province> getAllProvince() {
+
+        return provinceService.getAllProvinces();
+    }
+
+    @PostMapping("/api/gimnasio/province")
+    public void addProvince (@RequestBody Province province) {
+        provinceService.addProvince(province);
+    }
+
+    @DeleteMapping("/api/gimnasio/province/{id}")
+    public void deleteProvince (@PathVariable Long id) {
+        provinceService.deleteProvince(id);
+    }
 
 }
