dd95dd4afce06d4ad0a15784fcb366255baf5b16
Cristian Mansilla <cristianezequielmansilla@hotmail.com>
2023-08-28 22:46:37 -0300

    :sparkles: Add: GA-48 Add service and controller of suscription type

--- src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/services/SuscriptionTypeService.java
+++ src/main/java/ProyectoATLbootcamp/Proyecto_RedGimnasios/services/SuscriptionTypeService.java
@@ -1,8 +1,11 @@
 package ProyectoATLbootcamp.Proyecto_RedGimnasios.services;
 
+import ProyectoATLbootcamp.Proyecto_RedGimnasios.models.SuscriptionType;
 import ProyectoATLbootcamp.Proyecto_RedGimnasios.repository.SuscriptionTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SuscriptionTypeService {
@@ -10,5 +13,27 @@ public class SuscriptionTypeService {
     @Autowired
     SuscriptionTypeRepository suscriptionTypeRepository;
 
+    public List<SuscriptionType> getAllSuscriptionsTypes () {
+        return (List<SuscriptionType>) suscriptionTypeRepository.findAll();
+    }
+
+    public SuscriptionType getSuscriptionType (Long id) {
+        return suscriptionTypeRepository.findById(id).orElse(null);
+    }
+
+    public SuscriptionType addSuscriptionType (SuscriptionType suscriptiontype) {
+        return suscriptionTypeRepository.save(suscriptiontype);
+    }
+
+    public SuscriptionType updateSuscriptionType(Long id, SuscriptionType suscriptiontype){
+        if (suscriptionTypeRepository.existsById(id)){
+            suscriptiontype.setId(id);
+            return suscriptionTypeRepository.save(suscriptiontype);
+        }
+        return null;
+    }
+    public void deleteSuscriptionType(Long id) {
+        suscriptionTypeRepository.deleteById(id);
+    }
 
 }
